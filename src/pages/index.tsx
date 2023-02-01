b90b629b1ef511460eccfc962b4e70c86abd9f85
Gabriel Sousa <79923219+Gabriel-Sousa@users.noreply.github.com>
2023-02-01 19:12:40 -0300

    setting the base

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <div className="font-roboto">Hello Word!</div>
+    </>
+  )
+}
